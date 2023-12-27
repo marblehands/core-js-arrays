e0707c0cdba3722eafbcccc63884c3cd6f31ad1e
Anna Chebysheva <anna.chebysheva@gmail.com>
2023-12-27 19:16:55 +0100

    feat: getMovingSum task is done

--- src/arrays-tasks.js
+++ src/arrays-tasks.js
@@ -230,8 +230,15 @@ function toArrayOfSquares(arr) {
  *   [ 0, 0, 0, 0, 0]         => [ 0, 0, 0, 0, 0]
  *   [ 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 ] => [ 1, 3, 6, 10, 15, 21, 28, 36, 45, 55 ]
  */
-function getMovingSum(/* arr */) {
-  throw new Error('Not implemented');
+function getMovingSum(arr) {
+  const result = [];
+  let sum = 0;
+  arr.reduce((acum, currentvalue) => {
+    sum += currentvalue;
+    result.push(sum);
+    return acum;
+  }, 0);
+  return result;
 }
 
 /**
