fbca4212f0ac3fb691e15c2ba819e1c0fdc09948
Anna Chebysheva <anna.chebysheva@gmail.com>
2024-01-02 00:06:30 +0100

    feat: isSameLength task is done

--- src/arrays-tasks.js
+++ src/arrays-tasks.js
@@ -163,8 +163,9 @@ function getAverage(arr) {
  *    isSameLength(['orange', 'banana', 'cherry']) => true
  *    isSameLength(['cat', 'dog', 'elephant']) => false
  */
-function isSameLength(/* arr */) {
-  throw new Error('Not implemented');
+function isSameLength(arr) {
+  const strLength = arr[0].length;
+  return arr.every((item) => item.length === strLength);
 }
 
 /**
