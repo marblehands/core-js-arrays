3af85e5e2b0f270863a7bc971cb7194baa9b43cc
Anna Chebysheva <anna.chebysheva@gmail.com>
2024-01-02 03:04:24 +0100

    feat: getIndicesOfOddNumbers task is done

--- src/arrays-tasks.js
+++ src/arrays-tasks.js
@@ -459,8 +459,14 @@ function getIdentityMatrix(/* n */) {
  *    getIndicesOfOddNumbers([2, 4, 6, 8, 10]) => []
  *    getIndicesOfOddNumbers([11, 22, 33, 44, 55]) => [0, 2, 4]
  */
-function getIndicesOfOddNumbers(/* numbers */) {
-  throw new Error('Not implemented');
+function getIndicesOfOddNumbers(numbers) {
+  const newArr = numbers.map((item, index) => {
+    if (item % 2 !== 0) {
+      return index;
+    }
+    return -1;
+  });
+  return newArr.filter((item) => item !== -1);
 }
 
 /**
