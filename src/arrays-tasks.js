b9f4fe349343f965306ec087f0cfd72f8caa6ed8
Anna Chebysheva <anna.chebysheva@gmail.com>
2024-01-02 02:01:59 +0100

    feat: generateOdds task is done

--- src/arrays-tasks.js
+++ src/arrays-tasks.js
@@ -385,8 +385,8 @@ function createChunks(arr, chunkSize) {
  *    generateOdds(2) => [ 1, 3 ]
  *    generateOdds(5) => [ 1, 3, 5, 7, 9 ]
  */
-function generateOdds(/* len */) {
-  throw new Error('Not implemented');
+function generateOdds(len) {
+  return new Array(len).fill(1).map((item, index) => item + index * 2);
 }
 
 /**
